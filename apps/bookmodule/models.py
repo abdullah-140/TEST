c9711c94b7cde72ea7d3b08e28ba5bed69f5a1dd
abdullah-140 <abdullahkalawad@gmail.com>
2024-04-29 17:46:18 +0300

    add db

--- apps/bookmodule/models.py
+++ apps/bookmodule/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class PCS(models.Model):
+ id = models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
+ Processor = models.CharField(max_length = 50)
+ RAM = models.CharField(max_length = 50)
+ Storage = models.CharField(max_length = 50)
+ graphics_card = models.CharField(max_length = 50)
+ Motherboard	 = models.CharField(max_length = 50)
+ Power_Supply = models.CharField(max_length = 50)
+ Case = models.CharField(max_length = 50)
+
+
